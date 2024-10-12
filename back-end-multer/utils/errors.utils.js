db42b29f404f082801ca0c642087eada5b4cf61c
RomeoDev <136720281+Romeofresnel@users.noreply.github.com>
2024-10-12 13:40:36 +0200

    Delete back-end-multer directory

--- back-end-multer/utils/errors.utils.js
+++
@@ -1,8 +0,0 @@
-module.exports.uploadError=(err)=>{
-    const error={format:'',maxSize:''}
-
-    if(err.message.includes('invalde file')){
-        error.format='format incompatibe'
-    }
-    return error
-}
